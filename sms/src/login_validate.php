fc97c7c9e71531d4946d35522b5a7fa61a89cf87
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-02-21 07:44:51 +0530

    Comments included
    Comments included in the login_validate file.

--- sms/src/login_validate.php
+++ sms/src/login_validate.php
@@ -1,33 +1,41 @@
 <!-- 
-	Author:
-	 Project 
-	 Version
-	 Module / Page  
-	 Description
+	Author  : Ashika Jahir
+	Project :School Management System
+	Version : 1
+
+	Module / Page  : Login Module/Validation Page(Server Side Script)
+	Description	   : This Page takes the username and password from the user, validates and navigate to dashboard if they are authenticated as valid user,else 					stays in same page .
 
 
 -->
 
 
 <?php
-/*
-*  Class
 
-*  Description
-*  Extend / Inherit/ 
+/*
 *
+*  Class    		: Login (Base Class)
+*  Description 		: It creates an object every time the user login and do the page functionality
+*  
 */
 	class Login 
 	{
-		public $db = '';
+		public $db = ''; 
 		
+
+		//Constructor that performs db connectivity 
+
 		public function __construct() 
 		{	
 			//connecting to db - sms_wizara
 			$this->db = new PDO("mysql:host=localhost;dbname=sms_wizara","ashi", "ashikajahir");
 			$this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+			//calls the main function of class
 			$this->user_login();
 		}
+
+		//This function takes two inputs (username and password), validates and navigate accordingly.
 		public function user_login() 
 		{
 			
@@ -39,16 +47,23 @@
 			 }		
 			try 
 			{
+				//creates a PDO statement
 				$query = $this->db->prepare("SELECT * FROM user_login WHERE username = '$username' and password = '$password'"); //checking the table - user_login
+
+				//Executing the query
 				$query->execute();
+
+				//fetching the row(if it is valid user or else it does not fetch anything)
 				$row = $query->fetch();				
 			} 
 			catch (PDOException $e) 
 			{
 				echo $e->getMessage();
 			}
-			$query->closeCursor();
-			$db = null;
+			$query->closeCursor(); //frees up the connection to the server so that other SQL statements may be issued
+			$db = null; //unsetting the db value
+
+			//if else condition checks the validity and navigate
 			if(!empty($row))
 			{
 				//echo "Access Granted";
@@ -62,7 +77,7 @@
 				$template = 'login.html';
 
 			}
-			require_once($template);
+			require_once($template); //Moves to/Loads the corresponding page
 		}
 	}
 
