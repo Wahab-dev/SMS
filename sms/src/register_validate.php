39526afb15a5d574d0a2f082ab0c987b07472a75
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-02-21 07:15:32 +0530

    Regster Page backend
    Register form insert code

---
+++ sms/src/register_validate.php
@@ -0,0 +1,41 @@
+<?php
+	class Register
+	{
+		public $db = '';
+		public function __construct() 
+		{	
+			//connecting to db - sms_wizara
+			$this->db = new PDO("mysql:host=localhost;dbname=sms_wizara","ashi", "ashikajahir");
+			$this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			$this->register_user();
+		}
+		public function register_user() 
+		{
+			
+			$template = '';
+			if (!empty($_POST['username']) && !empty($_POST['password']))
+			 {
+				$username = $_POST['username'];
+				$password = $_POST['password'];
+			 }		
+			try 
+			{
+				$query = $this->db->prepare("INSERT INTO user_login (username, password)VALUES ('$username', '$password')"); 
+				$query->execute();
+				echo "New User successfully... Successfully Registered";
+				
+			} 
+			catch (PDOException $e) 
+			{
+				echo $e->getMessage();
+			}
+
+			$query->closeCursor(); //frees up the connection to the server so that other SQL statements may be issued, 
+			$db = null;
+			
+		}
+	}
+
+	$new_user = new Register();
+
+?>
