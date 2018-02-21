f5bb66b7e560a610905c5cb668af048ec83889a2
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-02-21 07:54:11 +0530

    Comments added
    Comments added in register validate page

--- sms/src/register_validate.php
+++ sms/src/register_validate.php
@@ -1,12 +1,34 @@
+<!-- 
+	Author  : Ashika Jahir
+	Project :School Management System
+	Version : 1
+
+	Module / Page  : Login Module/Register Validation Page(Server Side Script)
+	Description	   : This Page takes the username and password from the user and enetrs it into the database and echos a messsage in the browser.
+
+-->
+
 <?php
+
+/*
+*
+*  Class    		: Register (Base Class)
+*  Description 		: It creates an object every time a user register and do the page functionality
+*  
+*/
 	class Register
 	{
 		public $db = '';
+
+		//Constructor that performs db connectivity 
+
 		public function __construct() 
 		{	
 			//connecting to db - sms_wizara
 			$this->db = new PDO("mysql:host=localhost;dbname=sms_wizara","ashi", "ashikajahir");
 			$this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+			//calls the main function of class
 			$this->register_user();
 		}
 		public function register_user() 
@@ -20,8 +42,13 @@
 			 }		
 			try 
 			{
+				//creates the PDO statement
 				$query = $this->db->prepare("INSERT INTO user_login (username, password)VALUES ('$username', '$password')"); 
+
+				//executes the query
 				$query->execute();
+
+				//Display the message
 				echo "New User.....Successfully Registered";
 				
 			} 
@@ -36,6 +63,7 @@
 		}
 	}
 
-	$new_user = new Register();
+	$new_user = new Register();//Creates an object of class Register everytime a new user register.
+
 
 ?>
