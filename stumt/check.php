bcf2d5c1704f0400d5cae5423de66b39ebcb58b3
Priyadarshini9396 <36497013+Priyadarshini9396@users.noreply.github.com>
2018-03-09 23:46:26 +0530

    stumt
    this code under when click submit button it hit to database and fetched data and displays in bootsrap modal..

---
+++ stumt/check.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+			 
+				$name = $_POST['name'];
+				$gender = $_POST['gender'];
+				$age = $_POST['age'];
+				$email = $_POST['email'];
+				$place = $_POST['place'];
+				$contact = $_POST['contact'];
+				$college = $_POST['college'];
+			
+
+
+			echo $name;
+			echo $gender;
+			echo $age;
+			echo $email;
+			echo $place;
+			echo $contact;
+			echo $college;
+
+
+
+
+
+
+?>
---
+++ stumt/registration.php
@@ -0,0 +1,155 @@
+<!-- 
+	Author  : Ashika Jahir
+	Project :Muslim Students Meet
+	
+
+	Module / Page  : Register Page
+	Description	   : This Page gets the user details and insert in to db and send a mail to the registered user.
+-->
+<?php 
+
+
+/*
+*
+*  Class    		: Register (Base Class)
+*  Description 		: It creates an object every time a user register and do the page functionality
+*  
+*/
+
+
+class Register
+	{
+		public $db = '';
+
+		//Constructor that performs db connectivity 
+
+		public function __construct() 
+		{	
+			//connecting to db - sms_wizara
+			$this->db = new PDO("mysql:host=localhost;dbname=muslim_students_meet","ashi", "ashikajahir");
+			$this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			
+		}
+
+		//This function registers the user and send them a mail.
+
+		public function register_user() 
+		{
+
+			
+				$name = $_POST['name'];
+				$gender = "Female";
+				$age = $_POST['age'];
+				$email = $_POST['email'];
+				$place = $_POST['place'];
+				$contact = $_POST['contact'];
+				$college = $_POST['college'];
+
+				if(isset($_POST['gender']))
+				{
+					$gender = "Male";
+				}
+
+	
+				$subject = '';
+				$message = '';
+
+
+			 
+
+			 try 
+			{
+				//creates the PDO statement
+				$queryStr = $this->db->prepare("INSERT INTO register_details (name,gender,age,email,place,contact,college)VALUES (:name,:gender,:age,:email,:place,:contact,:college)"); 
+
+				//executes the insert query
+				$queryStr->execute(array('name' => $name,'gender' => $gender,'age' => $age, 'email' => $email,'place' => $place, 'contact' => $contact, 'college'=>$college));
+
+				$queryStr = $this->db->prepare("SELECT id FROM register_details WHERE email = :email");
+
+				$queryStr->execute(array('email' => $email));
+				$row = $queryStr->fetch(PDO::FETCH_ASSOC);
+				$id = $row['id'];
+
+
+				//Message to be sent in mail
+
+				$message = "Dear ".$name. ",You have been Successfully registered in MUSLIM STUDENTS MEET 2018. Your id " .$id ;
+
+				
+				//Code to send email using send grid is given here
+
+				if (filter_var($email, FILTER_VALIDATE_EMAIL))
+				{
+					
+					//email send grid code starts here
+
+					require_once ('SendGrid-API/vendor/autoload.php');
+
+					/*Post Data*/
+
+					/*Content*/
+					$from = new SendGrid\Email("Admin-Wizara", "admin@muslimstudentsmeet.in");
+					$subject = "Registration Confirm - MUSLIM STUDENTS MEET - 2018";
+					$to = new SendGrid\Email($name, $email);
+					$content = new SendGrid\Content("text/html", $message);
+
+					/*Send the mail*/
+					$mail = new SendGrid\Mail($from, $subject, $to, $content);
+					$apiKey = ('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');
+					$sg = new \SendGrid($apiKey);
+
+					/*Response*/
+					$response = $sg->client->mail()->send()->post($mail);
+					//var_dump($response);
+
+					//ends here
+					echo $message;
+				}
+
+				else
+					{
+						echo "Enter valid email";
+
+					} 
+
+
+
+				//echo "New User.....Successfully Registered with id...." .$id;
+
+				//print_r(json_encode($res));
+
+				
+			} 
+			catch (PDOException $e) 
+			{
+				$error_message = $e->getMessage();
+
+				if(strpos($error_message, "SQLSTATE[23000]") !== false)
+				{
+					echo "Email already regsitered... Please enter new email id";
+				}
+				else
+				{
+					echo $e->getMessage();
+				}
+			}
+
+
+
+		}
+	}
+
+	$new_user = new Register();//Creates an object of class Register everytime a new user register.*/
+	$new_user->register_user();
+
+	
+	//header("Location:studentform.html");
+	
+
+
+
+
+
+
+?>
